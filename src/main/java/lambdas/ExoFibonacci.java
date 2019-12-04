3ded6d07c1b29f83d20f3866f0f09cc51b910e14
benjamin <benjamin.bettan@gmail.com>
2019-12-05 00:59:23 +0100

    tmp

--- src/main/java/lambdas/ExoFibonacci.java
+++ src/main/java/lambdas/ExoFibonacci.java
@@ -3,7 +3,13 @@ package lambdas;
 import java.util.stream.Stream;
 
 public class ExoFibonacci {
+	public static int getFibonacciValue(int depth) {
+		return depth==0 ? 1 : Stream.iterate(1, i->i+1).limit(depth).reduce(0, (a,b) -> a+b);
+	}
+	
 	public static int getFibonacciSuite(int depth) {
 		return depth==0 ? 1 : Stream.iterate(1, i->i+1).limit(depth).reduce(0, (a,b) -> a+b);
 	}
+	
+	
 }
--- src/test/java/com/bbe/testCucumber/TestFibonacci.java
+++ src/test/java/com/bbe/testCucumber/TestFibonacci.java
@@ -10,49 +10,49 @@ public class TestFibonacci {
 	public void test0retourne1() {
 		int depth = 0;
 		int expected = 1;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 	
 	@Test
 	public void test1retourne1() {
 		int depth = 1;
 		int expected = 1;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 	
 	@Test
 	public void test2retourne3() {
 		int depth = 2;
 		int expected = 3;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 
 	@Test
 	public void test3retourne6() {
 		int depth = 3;
 		int expected = 6;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 	
 	@Test
 	public void test4retourne10() {
 		int depth = 4;
 		int expected = 10;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 	
 	@Test
 	public void test5retourne15() {
 		int depth = 4;
 		int expected = 10;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 
 	@Test
 	public void test6retourne21() {
 		int depth = 6;
 		int expected = 21;
-		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciSuite(depth),ExoFibonacci.getFibonacciSuite(depth)==expected);
+		Assert.assertTrue("Expected " + expected +"/"+ExoFibonacci.getFibonacciValue(depth),ExoFibonacci.getFibonacciValue(depth)==expected);
 	}
 	
 }
