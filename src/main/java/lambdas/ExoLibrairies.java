327b0ecac9aa10796879d20bd1dad496bdece426
BenjaminBettan <BenjaminBettan@DESKTOP-RDLI158>
2019-12-04 18:45:30 +0100

    tmp

--- src/main/java/lambdas/ExoLibrairies.java
+++ src/main/java/lambdas/ExoLibrairies.java
@@ -1,40 +1,39 @@
 package lambdas;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.Collections2;
 
 public class ExoLibrairies {
 
 	public static int solve(int solde, List<Integer> cost, List<Integer> quantity) {
-		List<Librairie> librairies = genereListeDeLibrairiesEtTrieLesParPrix(cost, quantity);
-		return calcul(solde, librairies);
+		List<Librairie> librairies = genereListeDeLibrairies(cost, quantity);
+		return solve(solde, librairies);
 	}
 
-	private static int calcul(int solde, List<Librairie> librairies) {
+	private static int solve(int solde, List<Librairie> librairies) {
 		int nbLivreQueJePeuxAcheter = 0;
-		
-		for (Librairie librairie : librairies) {
-			for (int i = 0; i < librairie.getNbLivres(); i++) {
-				if (solde - librairie.getPrice()<0) {//je n ai plus d argent
-					return nbLivreQueJePeuxAcheter;
-				}
-				else {
-					solde = solde - librairie.getPrice();
-					nbLivreQueJePeuxAcheter++;
-				}
-			}
+		int soldeCopy = solde;
+        Collection<List<Librairie>> permutations = Collections2.permutations(librairies);
+        
+        for (List<Librairie> list : permutations) {
+			
 		}
-		return nbLivreQueJePeuxAcheter;
+		
+        return nbLivreQueJePeuxAcheter;
 	}
 
-	private static List<Librairie> genereListeDeLibrairiesEtTrieLesParPrix(List<Integer> cost, List<Integer> quantity) {
+	private static List<Librairie> genereListeDeLibrairies(List<Integer> cost, List<Integer> quantity) {
 		List<Librairie> librairies = new ArrayList<>();
 		
 		for (int i = 0; i < quantity.size(); i++) {
 			librairies.add(new Librairie(quantity.get(i), cost.get(i)));
 		}
 		
-		librairies.sort(Comparator.comparing(Librairie::getPrice));
-		
 		return librairies;
 	}
+	
+	
 }
--- src/test/java/com/bbe/testCucumber/TestExoLibrairies.java
+++ src/test/java/com/bbe/testCucumber/TestExoLibrairies.java
@@ -7,30 +7,10 @@ import java.util.*;
 
 public class TestExoLibrairies {
 
-//	@Test
-//	public void test1() {
-//		List<Integer> cost = Arrays.asList(1);
-//		List<Integer> quantity = Arrays.asList(1);
-//		int solde = 1;
-//		int result = ExoLibrairies.solve(solde ,cost,quantity);
-//		int expected = 1;
-//		assertTrue("Expected : " + expected +" / "+result, result==expected);
-//	}
-//	
-//	@Test
-//	public void test2() {
-//		List<Integer> cost = Arrays.asList(2);
-//		List<Integer> quantity = Arrays.asList(2);
-//		int solde = 2;
-//		int result = ExoLibrairies.solve(solde ,cost,quantity);
-//		int expected = 2;
-//		assertTrue("Expected : " + expected +" / "+result, result==expected);
-//	}
-	
 	@Test
 	public void test3() {
-		List<Integer> cost = Arrays.asList(2,2,2,1,1,1);
-		List<Integer> quantity = Arrays.asList(1,2,3,1,2,3);
+		List<Integer> cost = Arrays.asList(1,2,3);
+		List<Integer> quantity = Arrays.asList(1,2,3);
 		int solde = 3;
 		int result = ExoLibrairies.solve(solde ,cost,quantity);
 		int expected = 3;
